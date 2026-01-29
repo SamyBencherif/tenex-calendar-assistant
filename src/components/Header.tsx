7f556a332b3c0f2597b6ff71002e0481ac8c0e6f
Samy Bencherif <samybencherif73@gmail.com>
2026-01-29 10:37:32 -0500

    Remove apps button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -256,9 +256,6 @@ const Header = () => {
               Sign In
             </button>
           )}
-          <button className="p-2 hover:bg-gray-100 rounded-full">
-            <Grid className="w-5 h-5 text-gray-600" />
-          </button>
           <div className="w-8 h-8 rounded-full bg-blue-600 flex items-center justify-center text-white text-sm font-medium">
             S
           </div>
